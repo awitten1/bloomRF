d1c1864066a0197c526178eb4bb89e61458e8137
awitten1 <awitten1@terpmail.umd.edu>
2023-04-10 09:53:19 -0400

    changes

--- src/bloomRF/bloomRF.h
+++ src/bloomRF/bloomRF.h
@@ -1,61 +1,67 @@
 #include "city/city.h"
 #include <vector>
+#include <iostream>
 
 namespace filters {
 
-struct BloomFilterRFParameters
-{
-    BloomFilterRFParameters(size_t filter_size_, size_t filter_hashes_, size_t seed_, uint16_t delta_);
+struct BloomFilterRFParameters {
+  BloomFilterRFParameters(size_t filter_size_, size_t filter_hashes_,
+                          size_t seed_, uint16_t delta_);
 
-    /// size of filter in bytes.
-    size_t filter_size;
-    /// number of used hash functions.
-    size_t filter_hashes;
-    /// random seed for hash functions generation.
-    size_t seed;
-    /// Delta: distance between layers.
-    uint16_t delta;
+  /// size of filter in bytes.
+  size_t filter_size;
+  /// number of used hash functions.
+  size_t filter_hashes;
+  /// random seed for hash functions generation.
+  size_t seed;
+  /// Delta: distance between layers.
+  uint16_t delta;
+};
+
+template <typename T> class BloomRF {
+public:
+  using UnderType = uint64_t;
+  using Container = std::vector<UnderType>;
+
+  explicit BloomRF(const BloomFilterRFParameters &params);
+
+  void add(T data);
+
+  bool find(T data);
+
+  bool findRange(T low, T high);
+
+  const Container &getFilter() const { return filter; }
+  Container &getFilter() { return filter; }
+
+private:
+  /// size -- size of filter in bytes.
+  /// hashes -- number of used hash functions.
+  /// seed -- random seed for hash functions generation.
+  /// delta -- distance between bloomRF layers.
+  explicit BloomRF(size_t size_, size_t hashes_, size_t seed_, uint16_t delta_);
+
+  /// Returns size in bits.
+  size_t numBits() { return 8 * sizeof(UnderType) * filter.size(); }
+
+  size_t bloomRFHash(T data, size_t i);
+
+  size_t hash(T data, size_t i);
+
+  size_t hashes;
+  size_t seed;
+  size_t words;
+  uint16_t delta;
+
+  Container filter;
 };
 
 template<typename T>
-class BloomRF {
-public:
-    using UnderType = uint64_t;
-    using Container = std::vector<UnderType>;
+size_t bloomRFSize(const BloomRF<T>& bf) {
+  std::cout << sizeof(T) << std::endl;
+    std::cout << bf.getFilter().size() << std::endl;
 
-    explicit BloomRF(const BloomFilterRFParameters& params);
-
-    void add(T data);
-
-    bool find(T data);
-
-    bool findRange(T low, T high);
-
-    const Container & getFilter() const { return filter; }
-    Container & getFilter() { return filter; }
-
-private:
-    /// size -- size of filter in bytes.
-    /// hashes -- number of used hash functions.
-    /// seed -- random seed for hash functions generation.
-    /// delta -- distance between bloomRF layers.
-    explicit BloomRF(size_t size_, size_t hashes_, size_t seed_, uint16_t delta_);
-
-    /// Returns size in bits.
-    size_t size() {
-        return 8 * sizeof(UnderType) * filter.size();
-    }
-
-    size_t bloomRFHash(T data, size_t i);
-
-    size_t hashes;
-    size_t seed;
-    size_t words;
-    uint16_t delta;
-
-    Container filter;
-
-};
+  return (bf.getFilter().size() * sizeof(T));
+}
 
 } // namespace filters
-
--- src/test/test_bloomrf.cpp
+++ src/test/test_bloomrf.cpp
@@ -1,11 +1,54 @@
 #include "bloomRF/bloomRF.h"
 
 #include <gtest/gtest.h>
+#include <limits>
+#include <random>
+#include <unordered_set>
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+using filters::BloomFilterRFParameters;
+using filters::BloomRF;
+
+uint64_t randomUint64() {
+  std::random_device rd;
+  std::mt19937_64 e2(rd());
+  std::uniform_int_distribution<uint64_t> dist(
+      0, std::numeric_limits<uint64_t>::max());
+
+  return dist(rd);
+}
+
+class BloomFilterTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    for (int i = 0; i < 10000; ++i) {
+      auto x = randomUint64();
+      s.insert(x);
+      bf.add(x);
+    }
+  }
+  std::unordered_set<uint64_t> s;
+  BloomRF<uint64_t> bf{BloomFilterRFParameters{12000, 7, 0, 4}};
+};
+
+TEST_F(BloomFilterTest, NoFalseNegatives) {
+  for (auto it = s.cbegin(); it != s.cend(); ++it) {
+    ASSERT_TRUE(bf.find(*it));
+  }
+}
+
+TEST_F(BloomFilterTest, FalsePositiveRateBound) {
+  int false_positives = 0;
+  int denominator = 1000000;
+  int true_positive = 0;
+  for (int i = 0; i < denominator; ++i) {
+    if (s.find(i) == s.end()) {
+      if (bf.find(i)) {
+        ++false_positives;
+      }
+    } else {
+      ++true_positive;
+    }
+  }
+  std::cout << static_cast<double>(false_positives) / static_cast<double>(denominator - true_positive) << std::endl;
+
 }
