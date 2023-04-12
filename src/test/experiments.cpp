9686c006fff1c01b9a96c0df2f83cec3c3560d3e
awitten1 <awitten1@terpmail.umd.edu>
2023-04-11 21:10:52 -0400

    experiments

---
+++ src/test/experiments.cpp
@@ -0,0 +1,43 @@
+
+#include "experiments.h"
+
+#include <iomanip>
+#include <random>
+
+void runExperimentsForUniform() {
+  std::random_device rd;
+  std::mt19937 mt{rd()};
+  auto genUniform = [=, uniform = std::uniform_int_distribution<>{
+                            0}]() mutable { return uniform(mt); };
+  ExperimentDriver<uint64_t, decltype(genUniform)> ed64U{
+      BloomFilterRFParameters{4000000, 11, 0, 4}, genUniform};
+
+  ed64U.doInserts(2000000);
+  double fp = ed64U.randomQuerys(30000);
+
+  std::cout << fp << std::endl;
+}
+
+void runExperimentsForNormal() {
+  std::random_device rd;
+  std::mt19937 mt{rd()};
+  std::normal_distribution<> normal{1ULL << 31, 1ULL << 29};
+  auto genNormal = [&]() mutable {
+    auto ret = normal(mt);
+    return ret;
+  };
+
+  ExperimentDriver<uint64_t, decltype(genNormal)> ed64N{
+      BloomFilterRFParameters{3000000, 11, 0, 4}, genNormal};
+
+  ed64N.doInserts(2000000);
+
+  auto fp = ed64N.randomQuerys(30000);
+
+  std::cout << fp << std::endl;
+}
+
+int main() {
+  runExperimentsForUniform();
+  runExperimentsForNormal();
+}
--- src/test/test_bloomrf.cpp
+++ src/test/test_bloomrf.cpp
@@ -1,57 +1,38 @@
-#include "bloomRF/bloomRF.h"
-
 #include <gtest/gtest.h>
+
 #include <iomanip>
 #include <limits>
 #include <random>
 #include <unordered_set>
 
+#include "bloomRF/bloomRF.h"
+
 using filters::BloomFilterRFParameters;
 using filters::BloomRF;
 
-uint64_t randomUint64() {
+uint64_t randomUniformUint64() {
   std::random_device rd;
   std::mt19937_64 e2(rd());
   std::uniform_int_distribution<uint64_t> dist(
       0, std::numeric_limits<uint64_t>::max());
-
   return dist(rd);
 }
 
-class BloomFilterTest : public ::testing::Test {
+class BloomFilterUniformTest : public ::testing::Test {
  protected:
   void SetUp() override {
     for (int i = 0; i < 10000; ++i) {
-      auto x = randomUint64();
+      auto x = randomUniformUint64();
       s.insert(x);
       bf.add(x);
     }
   }
   std::unordered_set<uint64_t> s;
-  BloomRF<uint64_t> bf{BloomFilterRFParameters{18000, 6, 0, 4}};
+  BloomRF<uint64_t> bf{BloomFilterRFParameters{16000, 6, 0, 3}};
 };
 
-TEST_F(BloomFilterTest, NoFalseNegatives) {
+TEST_F(BloomFilterUniformTest, NoFalseNegatives) {
   for (auto it = s.cbegin(); it != s.cend(); ++it) {
     ASSERT_TRUE(bf.find(*it));
   }
-}
-
-TEST_F(BloomFilterTest, FalsePositiveRateBound) {
-  int false_positives = 0;
-  int denominator = 10000;
-  int true_positive = 0;
-  for (int i = 0; i < denominator; ++i) {
-    if (s.find(i) == s.end()) {
-      if (bf.find(i)) {
-        ++false_positives;
-      }
-    } else {
-      ++true_positive;
-    }
-  }
-  auto fp = static_cast<double>(false_positives) / static_cast<double>(denominator - true_positive);
-
-  ASSERT_LE(fp, .01) << std::setprecision(4) << "False Positive Rate: " << fp;
-
 }
