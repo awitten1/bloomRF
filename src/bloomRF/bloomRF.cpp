bb6b3f03323c0be3c25dc6dd4240dd60ac9bc6db
Andrew Witten <awitten1@terpmail.umd.edu>
2023-06-04 13:41:12 -0400

    Improve advance checks and fix experiment bug (#12)
    This fixes a bug with the experiments (missing sort), rewrites the experiments, and also improves the performance of advance checks.

--- src/bloomRF/bloomRF.cpp
+++ src/bloomRF/bloomRF.cpp
@@ -53,6 +53,7 @@ UnderType BloomRfImpl<T, UnderType>::bloomRFRemainder(T data,
                                                       int wordPos) const {
   UnderType offset =
       (data >> shifts[i]) & ((UnderType{1} << (delta[i] - 1)) - 1);
+
   UnderType ret = (UnderType{1} << offset);
   assert(offset < (1 << (delta[i] - 1)));
 
@@ -184,9 +185,7 @@ bool BloomRfImpl<T, UnderType>::findRange(T lkey, T hkey) const {
         auto hash = hashToIndexAndBitMask(check.low, layer);
         if (filter[hash.first] & hash.second) {
           Checks check_for_interval{
-              lkey,
-              hkey,
-              {typename Checks::Check{check.low, check.high}}};
+              lkey, hkey, {typename Checks::Check{check.low, check.high}}};
           check_for_interval.advanceChecks(shifts[layer - 1], delta[layer - 1]);
           new_checks.concatenateChecks(check_for_interval);
         }
@@ -204,23 +203,34 @@ bool BloomRfImpl<T, UnderType>::findRange(T lkey, T hkey) const {
 }
 
 template <typename T, typename UnderType>
-void BloomRfImpl<T, UnderType>::Checks::advanceChecks(size_t shifts, size_t delta) {
+void BloomRfImpl<T, UnderType>::Checks::advanceChecks(size_t shifts,
+                                                      size_t delta) {
   T target_width = T{1} << shifts;
   std::vector<Check> new_checks;
+  assert(checks.size() == 1);
+
   for (const auto& check : checks) {
     assert(check.low < lkey || check.high > hkey);
-    T lower_limit = (std::max(lkey, check.low) / target_width) * target_width;
+
+    T bm_for_max = (T{1} << (shifts + delta - 1)) - 1;
+    T lower_limit = (std::max(check.low, lkey) / target_width) * target_width;
     T upper_limit = (std::min(hkey, check.high) / target_width) * target_width;
-    for (T counter = lower_limit; counter <= upper_limit && counter >= lower_limit; counter += target_width) {
+
+    for (T counter = lower_limit;
+         counter <= upper_limit && counter >= lower_limit;) {
       T curr_high = counter + target_width;
-      if (!new_checks.empty() &&
-               new_checks.back().low >> (shifts + delta - 1) ==
-                (counter >> (shifts + delta - 1)) && (curr_high <= hkey) &&
-               !(new_checks.back().low < lkey ||
-                 new_checks.back().high > hkey))  {
-        new_checks.back().high = static_cast<T>(curr_high - 1);
-      } else {
+      if (counter < lkey || curr_high > hkey) {
         new_checks.push_back({counter, static_cast<T>(curr_high - 1)});
+        counter = curr_high;
+      } else {
+        T next;
+        if (static_cast<T>(counter | bm_for_max) <= upper_limit) {
+          next = static_cast<T>(counter | bm_for_max);
+        } else {
+          next = upper_limit;
+        }
+        new_checks.push_back({counter, next});
+        counter = next + 1;
       }
     }
   }
@@ -294,6 +304,6 @@ template class BloomRfImpl<uint32_t>;
 template class BloomRfImpl<uint64_t>;
 template class BloomRfImpl<uint64_t, uint32_t>;
 
-} // namespace detail
+}  // namespace detail
 
 }  // namespace filters
--- src/test/experiments.cpp
+++ src/test/experiments.cpp
@@ -3,6 +3,7 @@
 #include "city/city.h"
 
 #include <cstddef>
+#include <functional>
 #include <iomanip>
 #include <iostream>
 #include <limits>
@@ -11,16 +12,15 @@
 
 namespace {
 
-constexpr uint64_t uint64_max = std::numeric_limits<uint64_t>::max();
+template <typename T>
+void runRangeExperiments(T interval_size,
+                         std::function<T()> d,
+                         std::function<T()> qkg,
+                         const std::string& msg) {
+  ExperimentDriver<T> ed64U{
+      BloomFilterRFParameters{4000000, 0, {7, 7, 7, 4, 4, 2, 2, 2}}, d, qkg};
 
-
-
-template <typename T, typename Generator>
-void runRangeExperiments(T interval_size, Generator d) {
-  static_assert(std::is_same_v<decltype(d()), T>);
-
-  ExperimentDriver<T, Generator> ed64U{
-    BloomFilterRFParameters{6000000, 0, {10, 8, 6, 6, 5, 5, 4, 3}}, d};
+  std::cout << "Running experiment: " << msg << std::endl;
 
   ed64U.doInserts(2000000);
   double fp = ed64U.randomRangeQuerys(100000, interval_size);
@@ -28,11 +28,15 @@ void runRangeExperiments(T interval_size, Generator d) {
   std::cout << fp << std::endl;
 }
 
-template <typename T, typename Generator>
-void runPointExperiments(Generator d) {
+template <typename T>
+void runPointExperiments(std::function<T()> d,
+                         std::function<T()> qkg,
+                         const std::string& msg) {
   static_assert(std::is_same_v<decltype(d()), T>);
-  ExperimentDriver<T, Generator> ed64U{
-    BloomFilterRFParameters{3200000, 0, {8, 8, 6, 6, 5, 5, 4, 3}}, d};
+  ExperimentDriver<T> ed64U{
+      BloomFilterRFParameters{3200000, 0, {8, 8, 6, 6, 5, 5, 4, 3}}, d, qkg};
+
+  std::cout << "Running experiment: " << msg << std::endl;
 
   ed64U.doInserts(2000000);
   double fp = ed64U.randomQuerys(100000);
@@ -40,27 +44,78 @@ void runPointExperiments(Generator d) {
   std::cout << fp << std::endl;
 }
 
+}  // namespace
+
+namespace {
+
+std::random_device rd;
+std::mt19937 gen(rd());
+
+uint64_t genNormalUInt(uint64_t mean, uint64_t stddev) {
+  uint64_t val = std::round(
+      std::normal_distribution<double>(mean, stddev)(gen));
+  // std::cout << val << std::endl;
+  return val;
 }
 
+uint64_t genUniformUInt(uint64_t low, uint64_t high) {
+  uint64_t val = std::round( // NOLINT
+      std::uniform_int_distribution<uint64_t>(low, high)(gen));
+  // std::cout << val << std::endl;
+  return val;
+}
+
+double genUniformDouble(double low, double high) {
+  return std::uniform_real_distribution<double>(low, high)(gen);
+}
+
+double genNormalDouble(double mean, double stddev) {
+  return std::normal_distribution<double>(mean, stddev)(gen);
+}
+
+}  // namespace
+
 int main() {
-  std::random_device rd;
-  std::mt19937 gen(rd());
-  runPointExperiments<uint64_t>([&]() mutable {
-    auto val = std::normal_distribution<double>(1 << 30, 1ULL << 40)(gen);
-    return static_cast<uint64_t>(val);
-  });
-  runPointExperiments<uint64_t>([&]() mutable {
-    return std::uniform_int_distribution<uint64_t>(0, std::numeric_limits<uint64_t>::max())(gen);
-  });
-  std::cout << "-----------RUNNING RANGE EXPERIMENTS-----------" << std::endl;
-  runRangeExperiments<uint64_t>(1e3, [&]() mutable {
-    auto val = std::normal_distribution<double>(1 << 30, 1ULL << 38)(gen);
-    return static_cast<uint64_t>(val);
-  });
-  runRangeExperiments<uint64_t>(1e7, [&]() mutable {
-    return std::uniform_int_distribution<uint64_t>(0, std::numeric_limits<uint64_t>::max())(gen);
-  });
-  runRangeExperiments<double>(1000000, [&]() mutable {
-    return std::uniform_real_distribution<double>(0, std::numeric_limits<double>::max())(gen);
-  });
+  std::cout << "-----------RUNNING POINT QUERY EXPERIMENTS-----------" << std::endl;
+  runPointExperiments<uint64_t>(
+      []() { return genNormalUInt(1ULL << 33, 1ULL << 31); },
+      []() { return genNormalUInt(1ULL << 33, 1ULL << 31); },
+      "point query, unsigned integer, normal distribution");
+
+  runPointExperiments<uint64_t>(
+      []() { return genUniformUInt(0, std::numeric_limits<uint64_t>::max()); },
+      []() { return genUniformUInt(0, std::numeric_limits<uint64_t>::max()); },
+      "point query, unsigned integer, uniform distribution");
+
+  std::cout << "-----------RUNNING RANGE QUERY EXPERIMENTS-----------" << std::endl;
+  runRangeExperiments<uint64_t>(
+      1e8, []() { return genNormalUInt(1ULL << 33, 1ULL << 31); },
+      []() { return genUniformUInt(0, std::numeric_limits<uint64_t>::max()); },
+      "unsigned integers, normal distriubtion, uniform query "
+      "keys.");
+
+  // This experiment has severely degraded performance (a completely untenable
+  // false positive rate).
+  runRangeExperiments<uint64_t>(
+      1e5, []() { return genNormalUInt(1ULL << 33, 1ULL << 31); },
+      []() { return genNormalUInt(1ULL << 33, 1ULL << 31); },
+      "unsigned integers, normal distriubtion, normal query "
+      "keys.");
+
+  runRangeExperiments<uint64_t>(
+      1e8,
+      []() { return genUniformUInt(0, std::numeric_limits<uint64_t>::max()); },
+      []() { return genUniformUInt(0, std::numeric_limits<uint64_t>::max()); },
+      "unsigned integers, uniform distribution for both inserts and queries.");
+
+  runRangeExperiments<double>(
+      10,
+      []() { return genUniformDouble(0, std::numeric_limits<double>::max()); },
+      []() { return genUniformDouble(0, std::numeric_limits<double>::max()); },
+      "floats, uniform distribution");
+
+  runRangeExperiments<double>(
+      1, []() { return genNormalDouble(1ULL << 32, 1ULL << 31); },
+      []() { return genNormalDouble(1ULL << 32, 1ULL << 31); },
+      "floats, normal distribution");
 }
